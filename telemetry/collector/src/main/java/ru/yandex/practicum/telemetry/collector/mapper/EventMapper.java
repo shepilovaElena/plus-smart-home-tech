560f533b6158d341cba411d05c8819f2ca9bf649
Shepilova <magic.hollow@yandex.kz>
2025-08-17 14:38:39 +0400

    ТЗ-19. fix mapper.

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/mapper/EventMapper.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/mapper/EventMapper.java
@@ -3,17 +3,17 @@ package ru.yandex.practicum.telemetry.collector.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
-import ru.yandex.practicum.kafka.telemetry.event.ClimateSensorEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.ClimateSensorAvro;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceAddedEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceRemovedEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
-import ru.yandex.practicum.kafka.telemetry.event.LightSensorEventAvro;
-import ru.yandex.practicum.kafka.telemetry.event.MotionSensorEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.LightSensorAvro;
+import ru.yandex.practicum.kafka.telemetry.event.MotionSensorAvro;
 import ru.yandex.practicum.kafka.telemetry.event.ScenarioAddedEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.ScenarioRemovedEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SwitchSensorAvro;
-import ru.yandex.practicum.kafka.telemetry.event.TemperatureSensorEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.TemperatureSensorAvro;
 import ru.yandex.practicum.telemetry.collector.model.hub.DeviceAddedEvent;
 import ru.yandex.practicum.telemetry.collector.model.hub.DeviceRemovedEvent;
 import ru.yandex.practicum.telemetry.collector.model.hub.ScenarioAddedEvent;
@@ -39,7 +39,7 @@ public interface EventMapper {
     @Mapping(target = "temperatureC", source = "temperatureC")
     @Mapping(target = "humidity", source = "humidity")
     @Mapping(target = "co2Level", source = "co2Level")
-    ClimateSensorEventAvro toPayload(ClimateSensorEvent event);
+    ClimateSensorAvro toPayload(ClimateSensorEvent event);
 
     @Mapping(target = "id", source = "id")
     @Mapping(target = "hubId", source = "hubId")
@@ -50,7 +50,7 @@ public interface EventMapper {
     @ToPayload
     @Mapping(target = "linkQuality", source = "linkQuality")
     @Mapping(target = "luminosity", source = "luminosity")
-    LightSensorEventAvro toPayload(LightSensorEvent event);
+    LightSensorAvro toPayload(LightSensorEvent event);
 
     @Mapping(target = "id", source = "id")
     @Mapping(target = "hubId", source = "hubId")
@@ -62,7 +62,7 @@ public interface EventMapper {
     @Mapping(target = "linkQuality", source = "linkQuality")
     @Mapping(target = "motion", source = "motion")
     @Mapping(target = "voltage", source = "voltage")
-    MotionSensorEventAvro toPayload(MotionSensorEvent event);
+    MotionSensorAvro toPayload(MotionSensorEvent event);
 
     @Mapping(target = "id", source = "id")
     @Mapping(target = "hubId", source = "hubId")
@@ -83,7 +83,7 @@ public interface EventMapper {
     @ToPayload
     @Mapping(target = "temperatureC", source = "temperatureC")
     @Mapping(target = "temperatureF", source = "temperatureF")
-    TemperatureSensorEventAvro toPayload(TemperatureSensorEvent event);
+    TemperatureSensorAvro toPayload(TemperatureSensorEvent event);
 
     @Mapping(target = "hubId", source = "hubId")
     @Mapping(target = "timestamp", source = "timestamp")
