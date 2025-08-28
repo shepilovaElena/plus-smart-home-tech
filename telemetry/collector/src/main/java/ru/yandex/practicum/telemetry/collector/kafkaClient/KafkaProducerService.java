bb8ac8f296af3d87ed73662af6ea6ddd96cf29cd
Shepilova <magic.hollow@yandex.kz>
2025-08-28 18:30:08 +0400

    ТЗ-19. refactor.

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/kafkaClient/KafkaProducerService.java
@@ -0,0 +1,48 @@
+package ru.yandex.practicum.telemetry.collector.kafkaClient;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import ru.yandex.practicum.telemetry.collector.model.sensor.SensorEvent;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Component
+public class KafkaProducerService {
+    public Producer<String, SensorEventAvro> createSensorEventProducer() {
+        return new KafkaProducer<>(addProperties());
+    }
+
+    public Producer<String, HubEventAvro> createHubEventProducer() {
+        return new KafkaProducer<>(addProperties());
+    }
+
+    private Properties addProperties() {
+        Properties appProperties = new Properties();
+        try (InputStream input = getClass().getClassLoader()
+                .getResourceAsStream("application")) {
+            if (input == null) {
+                throw new RuntimeException("Unable to find application");
+            }
+            appProperties.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading properties", e);
+        }
+
+        Properties kafkaProps = new Properties();
+        kafkaProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                appProperties.getProperty("kafka.producer.bootstrap-servers", "localhost:9092"));
+        kafkaProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                appProperties.getProperty("kafka.producer.key-serializer", StringSerializer.class.getName()));
+        kafkaProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                appProperties.getProperty("kafka.producer.value-serializer", StringSerializer.class.getName()));
+
+        return kafkaProps;
+    }
+}
--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/CollectorServiceImpl.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/service/CollectorServiceImpl.java
@@ -2,10 +2,13 @@ package ru.yandex.practicum.telemetry.collector.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.telemetry.collector.kafkaClient.CollectorKafkaProperties;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import ru.yandex.practicum.telemetry.collector.kafkaClient.KafkaProducerService;
+import ru.yandex.practicum.telemetry.collector.kafkaClient.Topics;
 import ru.yandex.practicum.telemetry.collector.model.hub.HubEvent;
 import ru.yandex.practicum.telemetry.collector.model.sensor.SensorEvent;
 
@@ -13,21 +16,21 @@ import ru.yandex.practicum.telemetry.collector.model.sensor.SensorEvent;
 @RequiredArgsConstructor
 @Slf4j
 public class CollectorServiceImpl implements CollectorService {
-
-    private final CollectorKafkaProperties collectorProps;
-    private final KafkaEventSender eventSender;
-    private final SensorEventHandler sensorHandler;
     private final HubEventHandler hubHandler;
+    private final SensorEventHandler sensorHandler;
+    private final KafkaProducerService producerService;
 
     @Override
     public void sendSensorEvent(SensorEvent sensorEvent) {
+        Producer<String, SensorEventAvro> sensorProducer = producerService.createSensorEventProducer();
         SensorEventAvro avro = sensorHandler.mapToAvro(sensorEvent);
-        eventSender.send(collectorProps.getTopics().getSensorsEvents(), avro.getHubId(), avro.getTimestamp().toEpochMilli(), avro);
+        sensorProducer.send(new ProducerRecord<>(Topics.HUB_TOPIC, avro.getHubId(), avro));
     }
 
     @Override
     public void sendHubEvent(HubEvent hubEvent) {
+        Producer<String, HubEventAvro> hubProducer = producerService.createHubEventProducer();
         HubEventAvro avro = hubHandler.mapToAvro(hubEvent);
-        eventSender.send(collectorProps.getTopics().getHubsEvents(), avro.getHubId(), avro.getTimestamp().toEpochMilli(), avro);
+        hubProducer.send(new ProducerRecord<>(Topics.SENSOR_TOPIC, avro.getHubId(), avro));
     }
 }
